4231f0d2ec230f883f22201c48cdb4ed1ff12959
Jaromir Kalab <jaromir.kalab@gmail.com>
2019-01-21 13:24:44 +0100

    migrace webu
    init commit

---
+++ Remeslne_truhlarstvi_a_tesarstvi_Petr_Pribyl/Petr_Pribyl_-_remeslne_truhlarstvi_a_tesarstvi_s_5_letou_zarukou_files/Petr_Pribyl_-_remeslne_truhlarstvi_a_tesarstvi_s_5_letou_zarukou.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140302
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Petr_Pribyl_-_remeslne_truhlarstvi_a_tesarstvi_s_5_letou_zarukou_files/Petr_Pribyl_-_remeslne_truhlarstvi_a_tesarstvi_s_5_letou_zarukouMoz.css')
+fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');fixupIECSS3Opacity('id2');fixupIECSS3Opacity('id3');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
